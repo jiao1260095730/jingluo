115ae48a9b6ee831934fa958904f172fc5ca28f9
jiaobin <1260095730@qq.com>
2020-04-01 20:58:55 +0800

    111

--- target/classes/DML.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM db_1 where id = '1';
-INSERT INTO `db_1` VALUES ('1', '1', '1', '1', '1', '1');
-
-DELETE From db_2 where id = '1';
-INSERT INTO `db_2` VALUES ('1', '1', '1', '1', '1', '1');
