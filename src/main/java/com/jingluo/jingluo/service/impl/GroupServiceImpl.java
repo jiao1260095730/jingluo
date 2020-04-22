dd5a4af28e0572ad1899badb56f6519d3dba0a9b
wuyaohui <wyh9126@163.com>
2020-04-22 19:32:25 +0800

    团队 修改  小改动

--- src/main/java/com/jingluo/jingluo/service/impl/GroupServiceImpl.java
+++ src/main/java/com/jingluo/jingluo/service/impl/GroupServiceImpl.java
@@ -34,10 +34,11 @@ public class GroupServiceImpl implements GroupService {
 
     @Autowired
     private IdCode idCode;
-   //IdCode idCode = new IdCode();
+    //IdCode idCode = new IdCode();
 
     /**
      * 创建团队
+     *
      * @param group
      * @param stuCode
      * @return
@@ -45,25 +46,24 @@ public class GroupServiceImpl implements GroupService {
     @Override
     public ResultInfo groupCreate(Group group, String stuCode) {
 
-
         try {
             //获取学生个人信息
             Student stu = studentMapper.selectByCode(stuCode);
-            if (stu == null){
+            if (stu == null) {
                 return ResultInfo.fail("学生信息不能为空");
             }
             StudentGroup sg = new StudentGroup();
 
             //判断学生是否加入过团队
-            if (stu.getGroupId() == null){
+            if (stu.getGroupId() == null) {
                 //团队id
                 group.setGroupId(idCode.id());
                 //所在班级
                 group.setClassId(stu.getClassId());
                 //团队成员加 1
-                group.setStuNum(group.getStuNum()+1);
+                group.setStuNum(group.getStuNum() + 1);
                 //创建时间
-               // group.setCreateTime(new Date());
+                // group.setCreateTime(new Date());
 
                 int count2 = groupMapper.insertSelective(group);
                 //学生团队关联
@@ -72,12 +72,12 @@ public class GroupServiceImpl implements GroupService {
                 sg.setStuGroId(idCode.id());
                 int count = groupMapper.insertGroupStudent(sg);
 
-                if (count ==1 && count2 ==1){
+                if (count == 1 && count2 == 1) {
                     return ResultInfo.success("团队创建成功");
-                }else {
+                } else {
                     return ResultInfo.fail("团队创建失败l");
                 }
-            }else {
+            } else {
                 return ResultInfo.fail("团队创建失败");
             }
         } catch (Exception e) {
@@ -88,6 +88,7 @@ public class GroupServiceImpl implements GroupService {
 
     /**
      * 删除团队
+     *
      * @param groupId
      * @return
      */
@@ -96,9 +97,9 @@ public class GroupServiceImpl implements GroupService {
         int count = groupMapper.deleteGroup(groupId);
         int count2 = groupMapper.deleteStudentGroup(groupId);
 
-        if (count == 1 && count2 ==1){
+        if (count == 1 && count2 == 1) {
             return ResultInfo.success("删除成功");
-        }else{
+        } else {
             return ResultInfo.fail("删除失败");
         }
 
@@ -106,6 +107,7 @@ public class GroupServiceImpl implements GroupService {
 
     /**
      * 删除团队中的成员
+     *
      * @param groupId
      * @param studentId
      * @return
@@ -127,6 +129,7 @@ public class GroupServiceImpl implements GroupService {
 
     /**
      * 添加成员
+     *
      * @param groupId
      * @param studentId
      * @return
@@ -138,11 +141,11 @@ public class GroupServiceImpl implements GroupService {
 
             Group group = groupMapper.findGroupId(groupId);
             StudentGroup sg = new StudentGroup();
-            if (group != null){
+            if (group != null) {
                 //成员加一
                 group.setStuNum(group.getStuNum() + 1);
                 groupMapper.updateByPrimaryKey(group);
-            }else {
+            } else {
                 return ResultInfo.fail("没有这个团队");
             }
 
@@ -152,57 +155,58 @@ public class GroupServiceImpl implements GroupService {
             sg.setStuGroId(idCode.id());
             groupMapper.insertGroupStudent(sg);
 
-            return  ResultInfo.success("添加成员成功");
+            return ResultInfo.success("添加成员成功");
         } catch (Exception e) {
             e.printStackTrace();
             return ResultInfo.fail("添加成员失败");
         }
 
-
     }
 
     /**
      * 通过 班级名称 团队名称查询 团队
+     *
      * @param selectName
      * @param type
      * @return
      */
     @Override
-    public ResultInfo selectGroup(String selectName,String type) {
+    public ResultInfo selectGroup(String selectName, String type) {
         // 0 表示班级名称
-        if (type.equals("0")){
+        if (type.equals("0")) {
             Class aClass = classMapper.selectById(selectName);
             List<Group> group = groupMapper.selectGroupByClassId(aClass.getClassId());
-            if (group != null){
-                return ResultInfo.fail("查询成功",group);
-            }else {
+            if (group != null) {
+                return ResultInfo.fail("查询成功", group);
+            } else {
                 return ResultInfo.fail("其输入正确的班级");
             }
         }
         //1 表示团队名称
-        if (type.equals("1")){
-           Group group = groupMapper.selectGroupByGroupName(selectName);
-           if (group != null){
-               return ResultInfo.fail("查询成功",group);
-           }else {
-               return ResultInfo.fail("其输入正确的团队名称");
-           }
+        if (type.equals("1")) {
+            Group group = groupMapper.selectGroupByGroupName(selectName);
+            if (group != null) {
+                return ResultInfo.fail("查询成功", group);
+            } else {
+                return ResultInfo.fail("其输入正确的团队名称");
+            }
         }
         return ResultInfo.fail("查询失败,请正确输入要搜索的名称");
     }
 
     /**
      * 修改团队信息
+     *
      * @param group
      * @return
      */
     @Override
     public ResultInfo updataGroup(Group group) {
 
-        int count = groupMapper.updateByPrimaryKey(group);
-        if (count == 1){
+        int count = groupMapper.updateByPrimaryKeySelective(group);
+        if (count == 1) {
             return ResultInfo.success("修改成功");
-        }else {
+        } else {
             return ResultInfo.fail("修改失败");
         }
 
